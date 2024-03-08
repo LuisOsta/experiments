2d8e8d90197140a19373355865828502f2479b62
Luis Osta <luis.osta@wondersco.com>
2024-03-08 10:46:40 -0600

    wip progress

--- lockfree-hashmap/src/main.rs
+++ lockfree-hashmap/src/main.rs
@@ -3,7 +3,6 @@ use std::hash::Hasher;
 use std::sync::atomic::{AtomicPtr, Ordering};
 use std::sync::{Arc, Mutex};
 use std::{ptr, thread};
-
 const INITIAL_CAPACITY: usize = 16;
 
 struct Node<K, V> {
@@ -27,7 +26,7 @@ pub struct ConcurrentHashMap<K, V> {
  */
 impl<K, V> ConcurrentHashMap<K, V>
 where
-    K: Eq + std::hash::Hash,
+    K: Eq + std::hash::Hash + Clone,
     V: Clone,
 {
     pub fn new() -> Self {
@@ -43,25 +42,79 @@ where
     }
 
     pub fn insert(&self, key: K, value: V) {
-        let hash = self.hash(&key);
-        let index: usize = hash % self.capacity;
+        let index = self.get_bucket_slot(&key);
         let new_node = Box::into_raw(Box::new(Node {
-            key,
+            key: key.clone(),
             value,
             next: AtomicPtr::new(ptr::null_mut()),
         }));
 
         let mut head = self.buckets[index].load(Ordering::SeqCst);
-        loop {
-            unsafe { (*new_node).next.store(head, Ordering::SeqCst) };
 
+        // How do you enforce uniqueness
+        // Keep track of head pointer & iterate through the list to see if your key value is already there
+        // If found then compare_exchange that pointer, otherwise compare_exchange head pointer as is
+        // If fail to compare exchange, repeat algo
+
+        let mut prev = ptr::null_mut::<Node<K, V>>();
+        let mut current = head;
+
+        while !current.is_null() {
+            let node = unsafe { &*current };
+            let next_node = node.next.load(Ordering::SeqCst);
+            if node.key.eq(&key) {
+                // Change the next value of new node
+                unsafe { (*new_node).next.store(next_node, Ordering::SeqCst) };
+
+                // If prev is equal to null it means that the key of `head` is the same as the key of new_node
+                if prev.is_null() {
+                    match self.buckets[index].compare_exchange(
+                        head,
+                        new_node,
+                        Ordering::SeqCst,
+                        Ordering::SeqCst,
+                    ) {
+                        Ok(_) => return, // Successfully inserted
+                        Err(new_head) => {
+                            // The value in buckets[index] was updated in between the load and compare_exchange and you must restart
+                            current = new_head;
+                            head = new_head;
+                            continue;
+                        }
+                    }
+                } else {
+                    // Set prev.next to new_node if prev is not null.
+                    let prev_node = unsafe { &*prev };
+
+                    match prev_node.next.compare_exchange(
+                        current,
+                        new_node,
+                        Ordering::SeqCst,
+                        Ordering::SeqCst,
+                    ) {
+                        Ok(_) => return, // Successfully inserted
+                        Err(new_head) => {
+                            // The value in buckets[index] was updated in between the load and compare_exchange and you must restart
+                            current = new_head;
+                            head = new_head;
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            prev = current;
+            current = node.next.load(Ordering::SeqCst);
+        }
+
+        loop {
             //new node: k: 3, value 5
             //head: k 2, value 5, next: k: 3, value 10
             //k=3, k=2, k=3
-            // How do you enforce uniqueness
-            // Keep track of head pointer & iterate through the list to see if your key value is already there
-            // If found then compare_exchange that pointer, otherwise compare_exchange head pointer as is
-            // If fail to compare exchange, repeat algo
+
+            // In the case that the key value does not already exist in the linkedlist in the bucket at `index`.
+            // Then add the new_node to the front of the linkedlist and update the pointer at buckets[index]
+            unsafe { (*new_node).next.store(head, Ordering::SeqCst) };
             match self.buckets[index].compare_exchange(
                 head,
                 new_node,
@@ -85,18 +138,70 @@ where
         }
     }
 
+    fn insert_existing(&self, new_node: *mut Node<K, V>, index: usize) -> Result<(), ()> {
+        let key = unsafe { &(*new_node).key };
+        let head = self.buckets[index].load(Ordering::SeqCst);
+        let mut prev = ptr::null_mut::<Node<K, V>>();
+        let mut current = head;
+
+        while !current.is_null() {
+            let node = unsafe { &*current };
+            let next_node = node.next.load(Ordering::SeqCst);
+            if node.key.eq(&key) {
+                // Change the next value of new node
+                unsafe { (*new_node).next.store(next_node, Ordering::SeqCst) };
+
+                // If prev is equal to null it means that the key of `head` is the same as the key of new_node
+                if prev.is_null() {
+                    match self.buckets[index].compare_exchange(
+                        head,
+                        new_node,
+                        Ordering::SeqCst,
+                        Ordering::SeqCst,
+                    ) {
+                        Ok(_) => return Ok(()), // Successfully inserted
+                        Err(new_head) => {
+                            // The value in buckets[index] was updated in between the load and compare_exchange and you must restart
+                            current = new_head;
+                            continue;
+                        }
+                    }
+                } else {
+                    // Set prev.next to new_node if prev is not null.
+                    let prev_node = unsafe { &*prev };
+
+                    match prev_node.next.compare_exchange(
+                        current,
+                        new_node,
+                        Ordering::SeqCst,
+                        Ordering::SeqCst,
+                    ) {
+                        Ok(_) => return Ok(()), // Successfully inserted
+                        Err(new_head) => {
+                            // The value in buckets[index] was updated in between the load and compare_exchange and you must restart
+                            current = new_head;
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            prev = current;
+            current = node.next.load(Ordering::SeqCst);
+        }
+        Err(())
+    }
+
     /**
      * We really don't want to return a reference as that's error prone and will make garbage collection much harder in the future
      * This is intentional append-only but we may need to add garbage collection in the future
      */
     pub fn get(&self, key: &K) -> Option<V> {
-        let hash = self.hash(&key);
-        let index = hash % self.capacity;
+        let index = self.get_bucket_slot(key);
         let mut current = self.buckets[index].load(Ordering::SeqCst);
 
         while !current.is_null() {
             let node = unsafe { &*current };
-
             if node.key.eq(key) {
                 return Some(node.value.clone());
             }
@@ -111,6 +216,11 @@ where
         let mut hasher = std::collections::hash_map::DefaultHasher::new();
         key.hash(&mut hasher);
         hasher.finish() as usize
+    }
+
+    fn get_bucket_slot(&self, key: &K) -> usize {
+        let hash = self.hash(&key);
+        return hash % self.capacity;
     }
 }
 
@@ -145,7 +255,7 @@ fn main() {
         for i in 0..1000 {
             let key = format!("key{}", i);
             if let Some(value) = map.get(&key) {
-                let v = value.clone();
+                let _v = value.clone();
                 r.lock().unwrap().insert(key);
             }
         }
