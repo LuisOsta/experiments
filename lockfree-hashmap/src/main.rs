736266baf1979f72bb78955ff7eb6ad9f4889b06
Luis Osta <luisosta1999@gmail.com>
2024-03-07 22:18:37 -0600

    initial lockfree

--- lockfree-hashmap/src/main.rs
+++ lockfree-hashmap/src/main.rs
@@ -1,105 +1,93 @@
-use std::sync::{Arc, RwLock};
-use std::thread;
-use std::time::Duration;
+use std::hash::Hasher;
+use std::ptr;
+use std::sync::atomic::{AtomicPtr, Ordering};
 
-use rand::Rng;
+const INITIAL_CAPACITY: usize = 16;
 
-const NUM_BUCKETS: usize = 100;
-
-struct BucketItem<K, V> {
+struct Node<K, V> {
     key: K,
-    value: Arc<V>,
-}
-#[derive(Clone)]
-pub struct LockFreeConcurrentHashMap<K, V>
-where
-    K: ToString + Eq + PartialEq,
-{
-    buckets: Vec<Arc<RwLock<Vec<BucketItem<K, V>>>>>,
+    value: V,
+    next: AtomicPtr<Node<K, V>>,
 }
 
-impl<K, V> LockFreeConcurrentHashMap<K, V>
+pub struct ConcurrentHashMap<K, V> {
+    buckets: Vec<AtomicPtr<Node<K, V>>>,
+    capacity: usize,
+}
+
+impl<K, V> ConcurrentHashMap<K, V>
 where
-    K: ToString + Eq + PartialEq,
+    K: Eq + std::hash::Hash,
 {
     pub fn new() -> Self {
-        let mut buckets = Vec::with_capacity(NUM_BUCKETS);
-
-        for _ in 0..NUM_BUCKETS {
-            buckets.push(Arc::new(RwLock::new(Vec::new())));
+        let mut buckets = Vec::with_capacity(INITIAL_CAPACITY);
+        for _ in 0..INITIAL_CAPACITY {
+            buckets.push(AtomicPtr::new(ptr::null_mut()));
         }
 
-        Self { buckets }
-    }
-    fn get_bucket_slot(&self, key: &K) -> usize {
-        format!("{:?}", key.to_string()).len() % NUM_BUCKETS
+        ConcurrentHashMap {
+            buckets,
+            capacity: INITIAL_CAPACITY,
+        }
     }
 
-    // Insert a key-value pair into the map
-    // No way to making sure there aren't duplicate keys
-    fn insert(&self, key: K, value: V) {
-        let hash = self.get_bucket_slot(&key);
-        let mut bucket = self.buckets[hash].write().unwrap();
-        bucket.push(BucketItem {
+    pub fn insert(&self, key: K, value: V) {
+        let hash = self.hash(&key);
+        let new_node = Box::into_raw(Box::new(Node {
             key,
-            value: Arc::new(value),
-        });
+            value,
+            next: AtomicPtr::new(ptr::null_mut()),
+        }));
+
+        loop {
+            let mut head = self.buckets[hash % self.capacity].load(Ordering::Acquire);
+            unsafe { (*new_node).next.store(head, Ordering::Relaxed) };
+
+            match self.buckets[hash % self.capacity].compare_exchange(
+                head,
+                new_node,
+                Ordering::Release,
+                Ordering::Relaxed,
+            ) {
+                Ok(_) => break, // Successfully inserted
+                Err(actual) => {
+                    head = actual;
+                    // Collision, retry
+                }
+            }
+        }
     }
 
-    // Get the value associated with a key
-    fn get(&self, key: &K) -> Option<Arc<V>> {
-        let hash = self.get_bucket_slot(key);
-        let bucket = self.buckets[hash].read().unwrap();
-        bucket
-            .iter()
-            .find(|bucket| &bucket.key == key)
-            .map(|b| b.value.clone())
+    pub fn get(&self, key: &K) -> Option<&V> {
+        let hash = self.hash(&key);
+        let mut current = self.buckets[hash % self.capacity].load(Ordering::Acquire);
+
+        while !current.is_null() {
+            let node = unsafe { &*current };
+
+            if node.key == *key {
+                return Some(&node.value);
+            }
+
+            current = node.next.load(Ordering::Acquire);
+        }
+
+        None
+    }
+
+    fn hash(&self, key: &K) -> usize {
+        let mut hasher = std::collections::hash_map::DefaultHasher::new();
+        key.hash(&mut hasher);
+        hasher.finish() as usize
     }
 }
 
 fn main() {
-    let concurrent_map = LockFreeConcurrentHashMap::new();
-    // Create a random number generator
+    let map = ConcurrentHashMap::new();
 
-    // Generate a random integer in the range [0, 100)
-    // Spawn multiple threads to insert values concurrently
-    let max_key = 10;
-    let map_ref = concurrent_map.clone();
+    map.insert("key1", "value1");
+    map.insert("key2", "value2");
 
-    let read_handle = thread::spawn(move || {
-        // Print the contents of the map
-        let mut rng = rand::thread_rng();
-
-        loop {
-            let random_key = rng.gen_range(0..max_key);
-
-            if let Some(value) = map_ref.get(&random_key) {
-                println!("Key: {}, Value: {}", random_key, value);
-            } else {
-                println!("Did not find value for key {}", random_key);
-            }
-            thread::sleep(Duration::from_millis(100));
-        }
-    });
-
-    let insert_handles: Vec<_> = (0..max_key)
-        .map(|i| {
-            let map_ref = concurrent_map.clone();
-            thread::spawn(move || {
-                let mut rng = rand::thread_rng();
-                let random_key = rng.gen_range(0..1000);
-
-                thread::sleep(Duration::from_millis(random_key));
-                map_ref.insert(i, i * i);
-                println!("Inserted key: {}", i);
-            })
-        })
-        .collect();
-
-    // Wait for all threads to finish
-    for handle in insert_handles {
-        handle.join().unwrap();
-    }
-
-    read_handle.join().unwrap();
+    println!("{:?}", map.get(&"key1")); // Some("value1")
+    println!("{:?}", map.get(&"key3")); // None
 }
